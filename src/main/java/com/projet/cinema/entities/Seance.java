4b5340431fd69b043beba3d18b7e88547334c902
Haja Andria <kantiiriley@gmail.com>
2022-02-06 15:22:52 -0500

    Modifying Seance

--- src/main/java/com/projet/cinema/entities/Seance.java
+++ src/main/java/com/projet/cinema/entities/Seance.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class Seance implements Serializable {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.TIME)
     private Date heureDebut;
     @OneToMany(mappedBy = "seance")
     private Collection<ProjectionFilm> projectionFilm;
